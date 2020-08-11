013bd0d345d2ae1fbfb9d9b16afc5f5e923b102a
Tiger <huynhho1412@outlook.com>
2020-08-11 19:50:01 +0700

    Updates

---
+++ precache-manifest.5fbec891da18a1d6b006cc5a1fb57d67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d88f67c3558cdef8567126e81ed6efc3",
+    "url": "/emoji-search/index.html"
+  },
+  {
+    "revision": "f10d38ab79dfd3d6181f",
+    "url": "/emoji-search/static/css/2.5861e1ce.chunk.css"
+  },
+  {
+    "revision": "8432124d4910ec7cae9b",
+    "url": "/emoji-search/static/css/main.65605343.chunk.css"
+  },
+  {
+    "revision": "f10d38ab79dfd3d6181f",
+    "url": "/emoji-search/static/js/2.5d1476a3.chunk.js"
+  },
+  {
+    "revision": "fa9f8d86bca7ba94baa2c0fa4755314d",
+    "url": "/emoji-search/static/js/2.5d1476a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8432124d4910ec7cae9b",
+    "url": "/emoji-search/static/js/main.dfcf6ff3.chunk.js"
+  },
+  {
+    "revision": "d44d1531576180c634c7",
+    "url": "/emoji-search/static/js/runtime-main.d8cd5ee4.js"
+  }
+]);
